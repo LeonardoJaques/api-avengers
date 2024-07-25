136caf88e6cfe62d95c842107b4d0325469c9fa0
Leonardo Jaques <jaques.projetos@outlook.com>
2024-07-25 08:46:50 -0300

    Initial commit

---
+++ src/test/kotlin/br/com/jaquesprojetos/LeoAvangersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jaquesprojetos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeoAvangersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
